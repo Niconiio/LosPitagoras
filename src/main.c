3630f1c7ac506f4e648e05346dc2fbb8133781e5
Niconiio <nicole.zunigalob@gmail.com>
2026-04-22 22:35:50 -0400

    Primera version basica del main

--- src/main.c
+++ src/main.c
@@ -4,6 +4,27 @@
 
 int main(){
 //Grafica el seno, el coseno, y el circulo unitario.
+  int opcion;
+  do{
+    printf("Seleccione que desea visualizar");
+    printf("1)Funcion seno");
+    printf("2)Funcion coseno");
+    printf("0)Salir");
+    printf("Opcion:");
+    scanf("%d ",&opcion);
+    if (opcion ==1){
+      fseno(struct point lista[], int *tam, int x_min, int x_max, float escala_y, char @)
+    }
+    else if(opcion==2){
+      fcoseno(struct point lista[], int *tam, int x_min, int x_max, float escala_y, char @)
+    }
+    else if (opcion == 0) {
+      printf("Termino del programa.\n");
+    }
+    else {
+      printf("Opción no válida.\n");
+    }
 
+  }while (opcion >0);
   return 0;
 }
