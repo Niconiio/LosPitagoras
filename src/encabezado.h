eb85a2317626a58d1b827130245d5c163f1bc049
Yeremi <yeremirod93@gmail.com>
2026-05-21 16:18:04 -0400

    POO: Clases  graf y funcion creadas, uso de la clase vector en estas últimas

--- src/encabezado.h
+++ src/encabezado.h
@@ -1,5 +1,6 @@
 #include<stdlib.h>
 #include<string>
+#include<vector>
 #define pi 3.14159
 
 #ifndef encabezado_h
@@ -10,6 +11,44 @@ extern int x_min, x_max, y_min, y_max;
 extern int tamano;
 
 
+class graf{
+  protected: 
+    std::vector<std::vector<int>> grafico_final;
+    int dom[2] = {0,0};
+    int rec[2] = {0,0};
+    char relleno;
+    std::vector<funcion> funciones;
+    int tam;
+  public:
+    graf(int y_max, int y_min,int  x_max, int x_min, char relleno): relleno(relleno){rec[0] = y_min; rec[1] = y_max; dom[0] = x_min; dom[1] = x_max;}
+    void add_funcion(funcion A){funciones.push_back(A);}
+   // void a(){graf_xy(struct point lista[], int tam,int y_max, int y_min, int  x_max, int x_min, char relleno)} //Usar la función, y luego aplicarla
+};
+
+
+class funcion{
+  protected:
+    int dom[2] = {0,0};
+    int rec[2] = {0,0};
+    char punto;
+    int escala;
+    std::vector<std::vector<int>> grafico;
+    int tam_graf;
+  public:
+    int Show_Tam(){tam_graf = grafico.size();return grafico.size();};
+    //virtual std::vector<std::vector<int>> funcion_() = 0;
+    virtual std::vector<std::vector<int>> funcion_();
+};
+
+//Espero que en la clase graficadora, pueda hacer interactuar varias funciones y sumarlas entre si, para poder crear ondas raras con senos y cosenos
+//La graficadora puede tener su propio vector de puntos de funciones, un vector final el cual grafica la función, resultante de realizar  operaciones entre los otros graficos
+
+//Gemini que es pair en C++
+//Puedo ordenar de menor a mayor los elementos de un vector apartir del primer elemento de un par en un vector de
+
+
+
+//Vector de point
 struct point{
   int x;
   int y;
